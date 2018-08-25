20f704d63bb8151540e0a0386d8b01f4e33e5bc4
bbbbb <bbb9870vvvv@gmail.com>
2018-08-25 09:45:50 +0000

    タスクコントローラー変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -54,7 +54,7 @@ private
 def set_task
   #@task = Task.find(params[:id])
   
-  @task = current_user.tasks.find_by(params[:id])
+  @task = current_user.tasks.find_by(id:params[:id])
   if (@task == nil)
     redirect_to root_url
   end
